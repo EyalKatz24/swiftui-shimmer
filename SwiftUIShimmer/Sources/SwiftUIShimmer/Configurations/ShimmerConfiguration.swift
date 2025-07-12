26539f88aad4b15d04f1682e686324ddebf5297d
Eyal Katz <eyalkatz2403@gmail.com>
2025-07-12 20:16:55 +0300

    Create ShimmerConfiguration.swift
    Would change for nicer API.

---
+++ SwiftUIShimmer/Sources/SwiftUIShimmer/Configurations/ShimmerConfiguration.swift
@@ -0,0 +1,14 @@
+//
+//  ShimmerConfiguration.swift
+//  SwiftUIShimmer
+//
+//  Created by Eyal Katz on 12/07/2025.
+//
+
+import SwiftUI
+
+public struct ShimmerConfiguration {
+    
+    var shapeColor: Color = .primary.opacity(0.2)
+    var shimmerColor: Color = .white.opacity(0.5)
+}
