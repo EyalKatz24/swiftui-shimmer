9343b83740dd1bb48a69e8cd2662e6b47a4e27b6
Eyal Katz <eyalkatz2403@gmail.com>
2025-07-12 20:44:39 +0300

    Create constants & configuration+colors.

---
+++ SwiftUIShimmer/Sources/SwiftUIShimmer/Base/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SwiftUIShimmer
+//
+//  Created by Eyal Katz on 12/07/2025.
+//
+
+import SwiftUI
+
+public enum Constants {
+    
+    public enum DefaultColors {
+        public static let shapeColor: Color = .primary.opacity(0.2)
+        public static let shimmerColor: Color = .white.opacity(0.6)
+    }
+}
---
+++ SwiftUIShimmer/Sources/SwiftUIShimmer/Configurations/ShimmerConfiguration+Colors.swift
@@ -0,0 +1,16 @@
+//
+//  ShimmerConfiguration+Colors.swift
+//  SwiftUIShimmer
+//
+//  Created by Eyal Katz on 12/07/2025.
+//
+
+import SwiftUI
+
+public extension ShimmerConfiguration {
+    
+    struct Colors {
+        var shapeColor: Color = Constants.DefaultColors.shapeColor
+        var shimmerColor: Color = Constants.DefaultColors.shimmerColor
+    }
+}
