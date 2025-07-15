781a117b2515a549c9e898298f656fef51cb80b0
Eyal Katz <eyalkatz2403@gmail.com>
2025-07-16 00:44:31 +0300

    Update ShimmerConfiguration+Colors.swift
    Documentations added.

--- Sources/SwiftUIShimmer/Configurations/ShimmerConfiguration+Colors.swift
+++ Sources/SwiftUIShimmer/Configurations/ShimmerConfiguration+Colors.swift
@@ -18,9 +18,24 @@ public extension ShimmerConfiguration {
         /// The color of the shimmer (highlight color).
         public let shimmerColor: Color
         
+        
+        /// Initializes a new `Colors` configuration for the shimmer effect.
+        ///
+        /// - Parameters:
+        ///   - shapeColor: The base color of the shape where the shimmer animates on and clipped to. Defaults to `Constants.DefaultColors.shapeColor`.
+        ///   - shimmerColor: The highlight color used for the shimmer effect. Defaults to `Constants.DefaultColors.shimmerColor`.
         public init(shapeColor: Color = Constants.DefaultColors.shapeColor, shimmerColor: Color = Constants.DefaultColors.shimmerColor) {
             self.shapeColor = shapeColor
             self.shimmerColor = shimmerColor
         }
     }
+}
+
+internal extension ShimmerConfiguration.Colors {
+    
+    /// A predefined color configuration for inner shapes.
+    ///
+    /// This configuration uses slightly lighter colors compared to the default ones.
+    /// It is intended for use in nested or inner shapes within a `Shimmer`.
+    static let innerShape: Self = .init(shapeColor: .innerShapeBase, shimmerColor: .innerShimmer)
 }
